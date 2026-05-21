db08f7b8ae0f7845ebc7769ddea92d420981371f
bluemoon-96 <yms6348@gmail.com>
2026-05-21 17:25:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE = 'futsal-v1';
-const FILES = ['./', './index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
+const STATIC = ['./manifest.json', './icon.svg', './icon-maskable.svg'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
   self.skipWaiting();
 });
 
@@ -16,7 +16,23 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  const url = new URL(e.request.url);
+  const isHtml = url.pathname.endsWith('/') || url.pathname.endsWith('.html');
+
+  if (isHtml) {
+    // index.html: 항상 네트워크 우선, 오프라인이면 캐시 사용
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    // 아이콘 등 정적 파일: 캐시 우선
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+  }
 });
